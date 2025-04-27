0715d40191272b66d5739ae437c37b340cc44db7
Obed tech <obedomoke18@gmail.com>
2025-04-27 08:45:17 +0300

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -41,7 +41,7 @@ const update = async (m, Matrix) => {
 
             // Fetch latest commit hash
             const { data: commitData } = await axios.get(
-                "https://api.github.com/repos/XdTechPro/JAWAD-MD/commits/main"
+                "https://api.github.com/repos/Trippleo1802/OBED-TECHX1/commits/main"
             );
             const latestCommitHash = commitData.sha;
 
@@ -84,7 +84,7 @@ const update = async (m, Matrix) => {
             await editMessage("```🔄 Replacing files...```");
 
             // Replace files while skipping important configs
-            const sourcePath = path.join(extractPath, "JAWAD-MD-main");
+            const sourcePath = path.join(extractPath, "OBED-TECHX-main");
             await copyFolderSync(sourcePath, process.cwd(), ['package.json', 'config.cjs', '.env']);
 
             // Update package.json with new commit hash
